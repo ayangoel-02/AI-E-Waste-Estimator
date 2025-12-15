2cca2fedc8bdc54f1ca687d7d8b83cf1ee2999d9
ayangoel-02 <ayangoel2@gmail.com>
2025-12-15 18:45:08 +0530

    Create api.py

---
+++ netlify/functions/api.py
@@ -0,0 +1,10 @@
+from mangum import Mangum
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+sys.path.append(str(BASE_DIR))
+
+from src.app import app
+
+handler = Mangum(app)
